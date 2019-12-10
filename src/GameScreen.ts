091a9bfef1d54e6d20c84a47ee78d55749abe196
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-10 13:17:59 +0100

    10-12, changed reference path in class SchoolParty and added a GameScreen class

---
+++ src/GameScreen.ts
@@ -0,0 +1,36 @@
+class GameScreen{
+
+    
+    protected readonly canvas: HTMLCanvasElement;
+    protected readonly ctx: CanvasRenderingContext2D;
+
+    public constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D){
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+
+    public draw(){}
+
+    /**
+     * Writes text to the canvas
+     * @param {string} text - Text to write
+     * @param {number} fontSize - Font size in pixels
+     * @param {number} xCoordinate - Horizontal coordinate in pixels
+     * @param {number} yCoordinate - Vertical coordinate in pixels
+     * @param {string} alignment - Where to align the text
+     * @param {string} color - The color of the text
+     */
+    protected writeTextToCanvas(
+        text: string,
+        fontSize: number = 20,
+        xCoordinate: number,
+        yCoordinate: number,
+        alignment: CanvasTextAlign = "center",
+        color: string = "white",
+    ) {
+        this.ctx.font = `${fontSize}px Minecraft`;
+        this.ctx.fillStyle = color;
+        this.ctx.textAlign = alignment;
+        this.ctx.fillText(text, xCoordinate, yCoordinate);
+    }
+}
--- src/schoolParty.ts
+++ src/schoolParty.ts
@@ -2,7 +2,7 @@
  * Class SchoolParty handles the level of schoolparty
  * in which our main character goes to a party and encounters a boy
  */
-/// <reference path="GameObject.ts"/>
-class SchoolParty extends GameObject {
+/// <reference path="GameScreen.ts"/>
+class SchoolParty extends GameScreen {
 
 }
