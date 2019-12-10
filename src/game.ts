a6a50f1fa047a39e82350e55faec944039202431
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-10 12:40:47 +0100

    added class keyboardListener and canvasElements in game class

---
+++ src/game.ts
@@ -0,0 +1,17 @@
+class Game {
+    //global attributes for canvas
+    //Readonly attributes are read-only. They can only be initialized in the constructor.
+    private readonly canvas: HTMLCanvasElement;
+    private readonly ctx: CanvasRenderingContext2D;
+
+    //Handles screen events
+    
+    private keyboardListener: KeyboardListener;
+
+    
+
+
+
+
+
+}
