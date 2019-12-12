2edf36f283582d1461fd567e823c6fa0ed295300
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-12 10:23:26 +0100

    12-12, source should now be up to date

--- src/schoolParty.ts
+++ src/schoolParty.ts
@@ -35,8 +35,8 @@ class SchoolParty extends GameScreen {
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
-        this.writeTextToCanvas(ctx, "SCENE ONE", 140, 500, 200);
-        this.writeTextToCanvas(ctx, "PRESS ESC TO CONTINUE", 30, 500, 400);
+        // this.writeTextToCanvas(ctx, "SCENE ONE", 140, 500, 200);
+        // this.writeTextToCanvas(ctx, "PRESS ESC TO EXIT", 30, 500, 400);
         const backgroundImage = "./assets/images/background/SchoolPartyBackground.jpg";
         this.loadImage(backgroundImage, this.drawBackgroundToScreen);
     }
