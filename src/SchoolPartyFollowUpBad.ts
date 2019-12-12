e3787ea16dff796aad0e9c6aac8654db4e7226a2
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-12 18:10:29 +0100

    12-12, changed a name from Kees to hij so that the kids can fill in what his name is to make them more involved in the story

--- src/SchoolPartyFollowUpBad.ts
+++ src/SchoolPartyFollowUpBad.ts
@@ -20,8 +20,8 @@ class SchoolPartyFollowUpBad extends GameScreen {
         //Add a super constructor which will handle the level when it is drawn
         super(game, canvas, ctx, img, keyboardListener);
 
-        
-       
+
+
     }
 
     /**
@@ -34,20 +34,19 @@ class SchoolPartyFollowUpBad extends GameScreen {
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
-        this.writeTextToCanvas(ctx, "Je kijkt naar Kees, geeft hem een glimlach en geeft hem je snapchat.", 50, this.canvas.width/2, 200);
+        this.writeTextToCanvas(ctx, "Je kijkt naar hem, geeft hem een glimlach en geeft hem je snapchat.", 50, this.canvas.width / 2, 200);
         this.writeTextToCanvas(ctx, "Jij: Kijk eens aan, jongen. Met al die spieren die je hersenen pletten ben je hopelijk nog niet te dom om te lezen, hé? ", 30, this.canvas.width / 2, 400);
         this.writeTextToCanvas(ctx, "Hij: Hah! Jij kan praten. Ik kan tenminste de letters in mijn wiskundeopdrachten begrijpen. *typt naam over* ", 30, this.canvas.width / 2, 430);
         this.writeTextToCanvas(ctx, "Jij: Goed punt. Jij bent altijd nog te snel voor mij, ouwe. ", 30, this.canvas.width / 2, 460);
         this.writeTextToCanvas(ctx, "Hij: Oh, wacht jij maar tot je in de 4e zit. Ik ben al zo oud dat mijn botten kraken. *stuurt verzoek op snapchat* ", 30, this.canvas.width / 2, 490);
         this.writeTextToCanvas(ctx, "Jij: Nou, ik ga maar weer eens naar huis. Ik denk niet dat mijn moeder het leuk zal vinden dat ik hier ben geweest. ", 30, this.canvas.width / 2, 520);
         this.writeTextToCanvas(ctx, "jij: Ik spreek je later nog wel. Verlies je leesbrilletje niet voordat we kunnen praten op snapchat, opa! *lacht* ", 30, this.canvas.width / 2, 550);
-        this.writeTextToCanvas(ctx, "Hij: Zal ik zeker niet doen! Ik zie je morgen wel. Misschien stuur ik je straks nog wel een berichtje. ", 30, this.canvas.width / 2, 590);        
+        this.writeTextToCanvas(ctx, "Hij: Zal ik zeker niet doen! Ik zie je morgen wel. Misschien stuur ik je straks nog wel een berichtje. ", 30, this.canvas.width / 2, 590);
         this.writeTextToCanvas(ctx, "Jij: Is goed! Doei!  ", 30, this.canvas.width / 2, 620);
         this.writeTextToCanvas(ctx, "Hij: Doei!  ", 30, this.canvas.width / 2, 650);
 
-
         //     const backgroundImage = "./assets/images/background/SchoolPartyBackground.jpg";
-    //     this.loadImage(backgroundImage, this.drawBackgroundToScreen);
-     }
+        //     this.loadImage(backgroundImage, this.drawBackgroundToScreen);
+    }
 
 }
