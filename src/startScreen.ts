14efd43e71ff9dda9b89000c1cc5943c37227b3e
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-16 10:18:16 +0100

    16-12, removed wack background on startScreen

--- src/startScreen.ts
+++ src/startScreen.ts
@@ -78,9 +78,9 @@ class StartScreen extends GameScreen {
         //This is ugly but it gives an idea
 
         //write the image functions
-        this.ctx.drawImage(this.characterImage, this.canvas.width / 2 - this.characterImage.width / 2, this.canvas.height / 2 - this.characterImage.height / 2);
-        this.ctx.drawImage(this.snapchatImage, 0, 0);
-        this.ctx.drawImage(this.instagramImage, 1500, 0);
+        // this.ctx.drawImage(this.characterImage, this.canvas.width / 2 - this.characterImage.width / 2, this.canvas.height / 2 - this.characterImage.height / 2);
+        // this.ctx.drawImage(this.snapchatImage, 0, 0);
+        // this.ctx.drawImage(this.instagramImage, 1500, 0);
 
         //write the text functions
         this.writeTextToCanvas(ctx, "You", 200, this.canvas.width / 2 , 500);
