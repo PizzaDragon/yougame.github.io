66bdc19c28d10395c93710dff251321c6b7efa02
LeslieScherbeijn <54664144+LeslieScherbeijn@users.noreply.github.com>
2019-12-19 22:04:07 +0100

    19-12, chamged the dialogue in bad a bit to fit better

--- src/Home/Bad/HomeBadDialogueOne.ts
+++ src/Home/Bad/HomeBadDialogueOne.ts
@@ -7,6 +7,7 @@ class HomeBadDialogueOne extends GameScreen {
   private keesImage: HTMLImageElement;
   private youImage: HTMLImageElement;
   private dialogueBar: HTMLImageElement;
+  private snapchatImage: HTMLImageElement;
 
 
 
@@ -29,11 +30,13 @@ class HomeBadDialogueOne extends GameScreen {
       this.keesImage = new Image();
       this.youImage = new Image();
       this.dialogueBar = new Image()
+      this.snapchatImage = new Image()
       //Now, set the src to start loading the image
       this.backgroundImage.src = './assets/images/background/homeBedroom2.jpg';
       this.keesImage.src = "./assets/images/characters/kees1.png";
       this.youImage.src = "./assets/images/characters/karakter2.png";
       this.dialogueBar.src = "./assets/images/background/dialoguebar.png"
+      //this.snapchatImage.src = "./assets/images/snapchat/"
   }
 
   /**
@@ -47,9 +50,10 @@ class HomeBadDialogueOne extends GameScreen {
 
   public draw(ctx: CanvasRenderingContext2D) {
       //Loads images into the DOM
-      this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height)
-      this.ctx.drawImage(this.dialogueBar, 0, this.canvas.height / 1.25, this.canvas.width, this.canvas.height)
-
+      this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height);
+      this.ctx.drawImage(this.dialogueBar, 0, this.canvas.height / 1.25, this.canvas.width, this.canvas.height);
+      //this.ctx.drawImage(this.snapchatImage, this.canvas.width / 2 - this.snapchatImage.width / 2, this.canvas.height / 2 - this.snapchatImage.height / 3)
+      
       //Loads text into the DOM
       //Add snapchat screenshots where he asks for nudes instead of text, in the dialogue bar, add the yes or no options.
       this.writeTextToCanvas(ctx, "Kees vraagt om nudes.", this.canvas.width / 2, this.canvas.height - 120);
