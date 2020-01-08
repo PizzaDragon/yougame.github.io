97cbb9fba1e998c797dc90796f7e7e84316b10f5
shoottowin1 <shoottowin11@gmail.com>
2020-01-08 13:47:47 +0100

    8-1, finalised school good

---
+++ src/School/Good/SchoolGoodPart2.ts
@@ -0,0 +1,61 @@
+/// <reference path='../../GameScreen.ts'/>
+class SchoolGoodPart2 extends GameScreen {
+
+    //add the global attributes
+    private shouldStartLevel: boolean = false;
+    private backgroundImage: HTMLImageElement;
+    private emilyImage: HTMLImageElement;
+    private youImage: HTMLImageElement;
+    private dialogueBar: HTMLImageElement;
+
+
+
+
+    //Add the constructor
+    public constructor(
+        game: Game,
+        canvas: HTMLCanvasElement,
+        ctx: CanvasRenderingContext2D,
+        img: HTMLImageElement,
+        keyboardListener: KeyboardListener,
+        name: string
+    ) {
+        //Add a super constructor which will handle the level when it is drawn
+        super(game, canvas, ctx, img, keyboardListener, name);
+
+
+        //Loads the backgroundImage into the memory
+        this.backgroundImage = new Image();
+        this.emilyImage = new Image();
+        this.youImage = new Image();
+        this.dialogueBar = new Image()
+        //Now, set the src to start loading the image
+        this.backgroundImage.src = './assets/images/background/klaslokaal1.jpg';
+        this.emilyImage.src = "./assets/images/characters/emily1.png";
+        this.youImage.src = "./assets/images/characters/karakter2.png";
+        this.dialogueBar.src = "./assets/images/background/dialoguebar.png"
+    }
+
+    /**
+     * method to initialize the screen
+     */
+    public listen(input: KeyboardListener) {
+        if (input.isKeyDown(KeyboardListener.KEY_ENTER)) {
+            this.shouldStartLevel = true
+        }
+    }
+
+    public draw(ctx: CanvasRenderingContext2D) {
+        //Loads images into the DOM
+        this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.emilyImage, -200, 0, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.youImage, 200, 0, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.dialogueBar, 0, this.canvas.height / 1.25, this.canvas.width, this.canvas.height)
+
+        //Loads text into the DOM
+        this.writeTextToCanvas(ctx, "Na een lange schooldag met hard werken, naar de muur staren en", this.canvas.width/2, this.canvas.height - 90);
+        this.writeTextToCanvas(ctx, "niet zeker weten wat 'prend du temps' betekent, lopen jullie naar huis.", this.canvas.width/2, this.canvas.height - 50);
+        this.writeTextToCanvas(ctx, "DRUK OP SPATIE OM VERDER TE GAAN", this.canvas.width/2, this.canvas.height - 10);
+    }
+
+}
