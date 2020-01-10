89cddc8faf47cd07665f2e16fef1e4f5f19a1890
PizzaDragon <hann0009@hz.nl>
2020-01-10 15:21:39 +0100

    10-1, barbad

--- src/Bar/Bad/BarBadEndingPart2.ts
+++ src/Bar/Bad/BarBadEndingPart2.ts
@@ -2,10 +2,7 @@
 class BarBadEndingPart2 extends GameScreen {
 
     //add the global attributes
-    private shouldStartLevel: boolean = false;
     private backgroundImage: HTMLImageElement;
-    private emilyImage: HTMLImageElement;
-    private youImage: HTMLImageElement;
     private dialogueBar: HTMLImageElement;
 
 
@@ -26,36 +23,22 @@ class BarBadEndingPart2 extends GameScreen {
 
         //Loads the backgroundImage into the memory
         this.backgroundImage = new Image();
-        this.emilyImage = new Image();
-        this.youImage = new Image();
         this.dialogueBar = new Image()
         //Now, set the src to start loading the image
         this.backgroundImage.src = './assets/images/background/darkbedroom.jpg';
-        this.emilyImage.src = "./assets/images/characters/emily1.png";
-        this.youImage.src = "./assets/images/characters/karakter2.png";
         this.dialogueBar.src = "./assets/images/background/dialoguebar.png"
-    }
-
-    /**
-     * method to initialize the screen
-     */
-    public listen(input: KeyboardListener) {
-        if (input.isKeyDown(KeyboardListener.KEY_ENTER)) {
-            this.shouldStartLevel = true
-        }
     }
 
     public draw(ctx: CanvasRenderingContext2D) {
         //Loads images into the DOM
         this.ctx.drawImage(this.backgroundImage, 0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.emilyImage, -200, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.youImage, 200, 0, this.canvas.width, this.canvas.height)
-        this.ctx.drawImage(this.dialogueBar, 0, this.canvas.height / 1.25, this.canvas.width, this.canvas.height)
+        this.ctx.drawImage(this.dialogueBar, 0, this.canvas.height / 1.35, this.canvas.width, this.canvas.height)
 
         //Loads text into the DOM
-        this.writeTextToCanvas(ctx, "Je wordt wakker in een pikzwarte ruimte.", this.canvas.width / 2, this.canvas.height - 130);
-        this.writeTextToCanvas(ctx, "Terwijl je ogen aan de eindeloze duisternis wennen, heb je snel door dat je vastgebonden zit aan een bed.", this.canvas.width / 2, this.canvas.height - 90);
-        this.writeTextToCanvas(ctx, "Uh oh.", this.canvas.width / 2, this.canvas.height - 50);
+        this.writeTextToCanvas(ctx, "Je wordt wakker in een pikzwarte ruimte.", this.canvas.width / 2, this.canvas.height - 170);
+        this.writeTextToCanvas(ctx, "Terwijl je ogen aan de eindeloze duisternis wennen,", this.canvas.width / 2, this.canvas.height - 130);
+        this.writeTextToCanvas(ctx, "heb je snel door dat je vastgebonden zit aan een bed.", this.canvas.width / 2, this.canvas.height - 90);
+        this.writeTextToCanvas(ctx, `${this.name}: Uh oh.`, this.canvas.width / 2, this.canvas.height - 50);
         this.writeTextToCanvas(ctx, "DRUK OP SPATIE OM VERDER TE GAAN", this.canvas.width / 2, this.canvas.height - 10);
 
     }
